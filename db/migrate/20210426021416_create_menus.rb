766a452639622d5692d891eea900444f392f265e
yoshi-1023 <yoshika.122355@gmail.com>
2021-04-26 11:27:36 +0900

    menuモデルの作成

---
+++ db/migrate/20210426021416_create_menus.rb
@@ -0,0 +1,11 @@
+class CreateMenus < ActiveRecord::Migration[6.0]
+  def change
+    create_table :menus do |t|
+      t.string :menu_name,  null: false
+      t.text :recipe,       null: false
+      t.string :serving,    null: false
+      t.references :user,   foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_062050) do
+ActiveRecord::Schema.define(version: 2021_04_26_021416) do
+
+  create_table "menus", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "menu_name", null: false
+    t.text "recipe", null: false
+    t.string "serving", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_menus_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_04_20_062050) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "menus", "users"
 end
