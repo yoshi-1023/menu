0dedd4e5632f33e87b17c1a21f567bb488b79de7
yoshi-1023 <yoshika.122355@gmail.com>
2021-04-26 11:57:54 +0900

    フォームオブジェクトの実装

---
+++ app/models/menu_food_stuff.rb
@@ -0,0 +1,14 @@
+class MenuFoodStuff
+  include ActiveModel::Model
+  attr_accessor :menu_name, :recipe, :serving, :user_id, :food_stuff_name, :quantity, :menu_id
+
+  with_options presence: true do
+    validates :menu_name
+    validates :recipe
+    validates :serving
+    validates :user_id
+    validates :food_stuff_name
+    validates :quantity
+    validates :menu_id
+  end
+end
